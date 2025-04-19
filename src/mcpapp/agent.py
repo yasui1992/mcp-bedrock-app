1899f143960475e16e01ed377ffc08816e844953
yasui1992 <yasui1992github@gmail.com>
2025-04-20 07:02:45 +0900

    [WIP] Use tool calling and output LLM's response (ignore mypy-checking)

--- src/mcpapp/agent.py
+++ src/mcpapp/agent.py
@@ -5,6 +5,7 @@ from typing import Final
 
 import boto3
 from mcp import ClientSession, Tool
+import more_itertools as miter
 from mypy_boto3_bedrock_runtime.type_defs import MessageTypeDef
 
 
@@ -15,8 +16,14 @@ class BedrockAgent:
     # TODO: Resolve hard-coding
     BEDROCK_MODEL_ID: Final[str] = "anthropic.claude-3-haiku-20240307-v1:0"
 
-    def __init__(self, mcp_session: ClientSession):
+    def __init__(
+        self,
+        mcp_session: ClientSession,
+        max_actions: int = 10
+    ):
         self.mcp_session = mcp_session
+        self.max_actions = max_actions
+
         self._llm_client = boto3.client("bedrock-runtime")
         self._tools: list[Tool] = []
 
@@ -27,14 +34,62 @@ class BedrockAgent:
         self._tools = result.tools
 
     async def ainvoke(self, text: str) -> AsyncGenerator[str, None]:
-        response = self._converse(text)
-
-        yield response["stopReason"]
-
-    def _converse(self, text: str):
+        is_end = False
         messages: list[MessageTypeDef] = [
             {"role": "user", "content": [ { "text": text } ]}
         ]
+
+        for _ in range(self.max_actions):
+            converse_response = self._converse(messages)
+            stop_reason = converse_response["stopReason"]
+
+            if stop_reason == "tool_use":
+                role = converse_response["output"]["message"]["role"]
+                content = converse_response["output"]["message"]["content"]
+
+                for item in content:
+                    k, v = miter.only(item.items()) 
+
+                    if k == "text":
+                        messages.append({
+                            "role": role,
+                            "content": [item]
+                        })
+                    elif k == "toolUse":
+                        messages.append({
+                            "role": role,
+                            "content": [item]
+                        })
+
+                        tool_response = await self.mcp_session.call_tool(
+                            v["name"],
+                            v["input"]
+                        )
+                        logger.debug(tool_response.model_dump_json())
+
+                        messages.append({
+                            "role": "user",
+                            "content": [{
+                                "toolResult": {
+                                    "toolUseId": v["toolUseId"],
+                                    "content": [{
+                                        "json": {
+                                            "text": miter.only(tool_response.content).text  # noqa: E501
+                                        }
+                                    }]
+                                }
+                            }]
+                        })
+
+            else:
+                yield miter.only(converse_response["output"]["message"]["content"])["text"]  # noqa: E501
+                is_end = True
+
+            if is_end:
+                break
+
+    def _converse(self, messages: list[MessageTypeDef]):
+        logger.debug(json.dumps(messages, ensure_ascii=False))
 
         # TODO: Resolve too deeply nested
         response = self._llm_client.converse(
@@ -55,6 +110,6 @@ class BedrockAgent:
                 ]
             }
         )
-        logger.debug(json.dumps(response))
+        logger.debug(json.dumps(response, ensure_ascii=False))
 
         return response
