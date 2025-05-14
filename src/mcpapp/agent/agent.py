cb915b876ec356b11d2227c5232ed202e3f3c727
yasui1992 <yasui1992github@gmail.com>
2025-05-14 20:41:35 +0900

    Revert "Update src/mcpapp/agent/agent.py"
    This reverts commit f98d73b6e916b7638083cdd61639a6cc5c55c0f5.

--- src/mcpapp/agent/agent.py
+++ src/mcpapp/agent/agent.py
@@ -57,16 +57,7 @@ class BedrockAgent:
         self.llm_client = llm_client
         self.max_actions = max_actions
 
-self.llm_client = llm_client
-        self.max_actions = max_actions
-
-        # import os
-        self._model_id = os.environ.get("BEDROCK_MODEL_ID")
-        if self._model_id is None:
-            raise ValueError("BEDROCK_MODEL_ID environment variable is not set")
-        self._tool_config = ToolConfig()
-
-    async def afetch_tools(self):
+        self._model_id = os.environ["BEDROCK_MODEL_ID"]
         self._tool_config = ToolConfig()
 
     async def afetch_tools(self):
